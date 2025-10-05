0378e9bfb3b263f273696141e72cb3a1e83eda27
CodeMaker46 <shikshak013@gmail.com>
2025-10-05 17:02:56 +0530

    cors allow

--- app/server/index.js
+++ app/server/index.js
@@ -12,7 +12,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 const port = process.env.PORT || 5001;
 
